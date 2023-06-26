37d533df18dc39c65243b3318f618ddaa713e699
Sonali Raj <98824103+Sonali3110@users.noreply.github.com>
2023-06-27 04:24:51 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,3 @@
+gsap.from('h1' , {duration: 1, y: '-100%', ease:'bounce' })
+gsap.from('h6' , {duration: 2, y: '-100vw', ease:'power2,in' })
+gsap.from('#p2' , {duration: 1, x: '-100vw', ease:'power2,in' })
